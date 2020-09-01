2e1fa2f8f8db7e5dc91832b49c9697cc7119183a
matt poast <matt.poast@gmail.com>
2020-09-01 16:11:11 -0600

    mover script

---
+++ Fall2020Game/Assets/Scripts/MoverScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class MoverScript : MonoBehaviour
+{
+    public float x,y,z;
+    public float speed = 5f;
+    void Start()
+    {
+        Debug.Log("Hello World!");
+    }
+    void Update()
+    {
+        x = Input.GetAxis("Horizontal")*speed*Time.deltaTime;
+        y = Input.GetAxis("Vertical")*speed*Time.deltaTime;
+        transform.Translate(x,y,z);
+    }
+}
