8c8e1b1b2b52dff9396f4f678ef67affadadb277
matt poast <matt.poast@gmail.com>
2020-09-02 10:20:00 -0600

    enemy script wip

---
+++ Fall2020Game/Assets/Scripts/Enemy.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public GameObject sphere;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        sphere.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Fall2020Game/Assets/Scripts/MoverScript.cs
+++ Fall2020Game/Assets/Scripts/MoverScript.cs
@@ -1,5 +1,4 @@
 ﻿using UnityEngine;
-
 public class MoverScript : MonoBehaviour
 {
     public float x,y,z;
