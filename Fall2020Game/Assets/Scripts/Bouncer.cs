d4dbd3985fad59d891d200b57f3d9653217a0132
matt poast <matt.poast@gmail.com>
2020-09-05 16:13:50 -0600

    "Bounce Script"

---
+++ Fall2020Game/Assets/Scripts/Bouncer.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+public class Bouncer : MonoBehaviour
+{
+    public GameObject player;
+    public int bounce = 5;
+    public void OnTriggerEnter(Collider other)
+    {
+        player.transform.Translate(0, bounce,0);
+    }
+}
