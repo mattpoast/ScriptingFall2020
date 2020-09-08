2c4b9ccb87574ecffeaf5ff528b0f139cf485ff9
matt poast <matt.poast@gmail.com>
2020-09-07 22:30:49 -0600

    "Key and Door" script

---
+++ Fall2020Game/Assets/Scripts/Key.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Key : MonoBehaviour
+{
+    public GameObject key;
+    public GameObject door;
+    void Start()
+    {
+        door.SetActive(true);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(key);
+        door.SetActive(false);
+    }
+}
