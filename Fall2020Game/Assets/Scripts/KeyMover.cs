80b615c6a45cd8ee8c7ee348e8099f15d9a7e94f
matt poast <matt.poast@gmail.com>
2020-09-08 19:04:05 -0600

    using help from internet, script to make an object bounce off walls

---
+++ Fall2020Game/Assets/Scripts/KeyMover.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+using UnityEngine;
+
+public class KeyMover : MonoBehaviour
+{
+    public float speed = 20f;
+    public Rigidbody rb;
+    public Vector2 bounceRight;
+    public Vector2 bounceLeft;
+    public GameObject wall1, wall2;
+    private void Start()
+    {
+        bounceRight = new Vector2(-5,0);
+        bounceLeft = new Vector2(5, 0);
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if(wall1)
+        {
+            rb.AddForce(speed * bounceLeft, ForceMode.Impulse);
+        }
+
+        if (wall2)
+        {
+            rb.AddForce(speed * bounceRight, ForceMode.Impulse);
+        }
+        
+    }
+}
