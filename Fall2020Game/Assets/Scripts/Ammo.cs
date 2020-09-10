2d6f2cdc3d0aa87e85f7b248cc3de0ebf85ffa0f
matt poast <matt.poast@gmail.com>
2020-09-10 17:18:45 -0600

    Ammo script

---
+++ Fall2020Game/Assets/Scripts/Ammo.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Ammo : MonoBehaviour
+{
+    public GameObject ammo;
+    public int ammoCount;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        gameObject.SetActive(false);
+        ammoCount += 5;
+    }
+}
