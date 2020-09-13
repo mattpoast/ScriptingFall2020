30c5f3e152147287f3609a4aa43174bb887d1c09
matt poast <matt.poast@gmail.com>
2020-09-12 21:26:03 -0600

    GameOver, player death script

---
+++ Fall2020Game/Assets/Scripts/DeathScreen.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+public class DeathScreen : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject deathScreen;
+    void Start()
+    {
+        deathScreen.SetActive(false);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        player.SetActive(false);
+        deathScreen.SetActive(true);
+        
+    }
+}
