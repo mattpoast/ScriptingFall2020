ca66ee9b60c695cdb6bdd7d76cedda75257fdd06
matt poast <matt.poast@gmail.com>
2020-09-13 23:14:18 -0600

    Speed Boots script

---
+++ Fall2020Game/Assets/Scripts/BootsScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class BootsScript : MonoBehaviour
+{
+    public GameObject boots;
+    public int speed = 5;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        boots.SetActive(false);
+        speed += 5;
+    }
+}
