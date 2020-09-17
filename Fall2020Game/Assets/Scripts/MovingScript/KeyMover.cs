ba1b9f4d0cb34a101af36ea338b57e1c08e3e292
matt poast <matt.poast@gmail.com>
2020-09-16 22:44:58 -0600

    trying to fix vector 3 script

--- Fall2020Game/Assets/Scripts/MovingScript/KeyMover.cs
+++ Fall2020Game/Assets/Scripts/MovingScript/KeyMover.cs
@@ -6,17 +6,16 @@ public class KeyMover : MonoBehaviour
 {
     public float speed = 5f;
     public Vector3 mover;
-
     public void Update()
     {
-        mover.x = speed * Time.deltaTime;
-      transform.Translate(mover);
-      if (mover.x < 2)
-      {
-          print("working");
-          transform.Translate(mover);
-          mover.y = speed;
-      }
-   
+        transform.Translate(mover);
+        if (mover.x < 2)
+        {
+            mover.x = speed * Time.deltaTime;
+        }
+        else
+        {
+            mover.x = -speed * Time.deltaTime;
+        }
     }
 }
