c1e4b9d74b3b40946c1a3a44ae252a4d26ebaac0
matt poast <matt.poast@gmail.com>
2020-09-18 23:23:01 -0600

    Fixed name of script so unity event will work

--- Fall2020Game/Assets/Scripts/UnityEvent.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-using UnityEngine;
-using UnityEngine.Events;
-
-public class UnityEvent : MonoBehaviour
-{
-    public UnityEvent myUnityEvent;
-}
---
+++ Fall2020Game/Assets/Scripts/UnityEvents.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class UnityEvents : MonoBehaviour
+{
+    public UnityEvent myUnityEvent;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        myUnityEvent.Invoke();
+    }
+}
