0c28d6d7254f95829e524a997f55d0d048158810
matt poast <matt.poast@gmail.com>
2020-09-19 16:27:52 -0600

    "restart Script"

---
+++ Fall2020Game/Assets/Scripts/Restart.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Restart : MonoBehaviour
+{
+    public Vector3 home;
+    public GameObject player;
+
+    private void Start()
+    {
+        home.x = 0;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("working");
+      player.transform.Translate(home);
+    }
+}
