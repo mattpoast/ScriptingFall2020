f83994c89a1066cec49667f98f52e8cfe744a9c3
matt poast <matt.poast@gmail.com>
2020-09-20 19:20:10 -0600

    password script

---
+++ Fall2020Game/Assets/Scripts/Password.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+public class Password : MonoBehaviour
+{
+    public string password;
+    public GameObject player;
+    void Start()
+    {
+        password = String.Empty;
+    }
+
+    private void Update()
+    {
+        if (password.Equals("please"))
+        {
+          print("working!!!");
+          player.transform.Translate(0, 5 * Time.deltaTime,0);
+        }
+    }
+}
