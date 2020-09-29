b18374b6e14a18c91f7309d1626d14ca2d238fe1
matt poast <matt.poast@gmail.com>
2020-09-28 23:39:19 -0600

    bullet script

---
+++ Fall2020Game/Assets/Scripts/MovingScript/Shooting.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public GameObject bullet;
+    public int bulletForce = 5;
+
+
+    private void OnMouseDown()
+    {
+        Instantiate(bullet);
+    }
+}
