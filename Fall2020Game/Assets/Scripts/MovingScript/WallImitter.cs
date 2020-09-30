c7646be48e7eb62a06e9216f8e8cf71ebaf5b4f5
matt poast <matt.poast@gmail.com>
2020-09-29 18:41:43 -0600

    instantiating script

---
+++ Fall2020Game/Assets/Scripts/MovingScript/WallImitter.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class WallImitter : MonoBehaviour
+{
+    public GameObject preFab;
+    public float delay = 5f;
+
+    IEnumerator Start()
+    {
+        while (true)
+        {
+            Instantiate(preFab);
+            yield return new WaitForSeconds(delay);
+        }
+    }
+}
