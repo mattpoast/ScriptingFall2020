31b458960179d47cc4ab43e370ebab3404011632
matt poast <matt.poast@gmail.com>
2020-10-02 23:27:44 -0600

    bullet script 2

--- Fall2020Game/Assets/Scripts/MovingScript/Shooting.cs
+++ Fall2020Game/Assets/Scripts/MovingScript/Shooting.cs
@@ -7,10 +7,15 @@ public class Shooting : MonoBehaviour
 {
     public GameObject bullet;
     public int bulletForce = 5;
-
+    public GameObject player;
 
     private void OnMouseDown()
     {
         Instantiate(bullet);
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+     bullet.SetActive(false);   
     }
 }
---
+++ Fall2020Game/Assets/Scripts/Randomizer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = System.Random;
+
+public class Randomizer : MonoBehaviour
+{
+    public GameObject cube, circle;
+    private string names;
+
+    private void Start()
+    {
+        Randomizer.Instantiate(cube);
+        Debug.Log("working");
+
+    }
+}
