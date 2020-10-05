f461d83e55a073c63914c9ef03e83a10badc6e38
matt poast <matt.poast@gmail.com>
2020-10-04 23:19:23 -0600

    shooter script to shoot and destroy bullet after timer

--- Fall2020Game/Assets/Scripts/MovingScript/Shooting.cs
+++ Fall2020Game/Assets/Scripts/MovingScript/Shooting.cs
@@ -1,21 +1,26 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Timers;
 using UnityEngine;
 
 public class Shooting : MonoBehaviour
 {
     public GameObject bullet;
     public int bulletForce = 5;
-    public GameObject player;
-
+    private float timer;
     private void OnMouseDown()
     {
         Instantiate(bullet);
+        bullet.transform.Translate(bulletForce, 0, 0);
     }
 
-    private void OnCollisionEnter(Collision other)
+    private void Update()
     {
-     bullet.SetActive(false);   
+        timer += 1f * Time.deltaTime;
+        if (timer >= 5f)
+        {
+            Destroy(bullet);
+        }
     }
 }
