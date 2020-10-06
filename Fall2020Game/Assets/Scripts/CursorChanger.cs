352ecd6125ce4973c8959b97d9fddf9df5e0baef
matt poast <matt.poast@gmail.com>
2020-10-05 20:39:30 -0600

    Custom cursor script, with little help from internet

---
+++ Fall2020Game/Assets/Scripts/CursorChanger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorChanger : MonoBehaviour
+{
+
+    public Texture2D defaultMouse;
+    public Vector2 hotSpot = Vector2.zero;
+     public CursorMode curMode = CursorMode.Auto;
+    void Start()
+    {
+        Cursor.SetCursor(defaultMouse, hotSpot, curMode);
+    }
+    
+}
