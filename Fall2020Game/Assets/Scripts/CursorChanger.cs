a133593e0e986c807ab7db5125bafa361bb3517f
matt poast <matt.poast@gmail.com>
2020-10-17 21:11:24 -0600

    script to change cursor on mouse over

--- Fall2020Game/Assets/Scripts/CursorChanger.cs
+++ Fall2020Game/Assets/Scripts/CursorChanger.cs
@@ -1,16 +1,19 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
+﻿using UnityEngine;
 public class CursorChanger : MonoBehaviour
 {
-
+    public Texture2D mouseOverMouse;
     public Texture2D defaultMouse;
     public Vector2 hotSpot = Vector2.zero;
-     public CursorMode curMode = CursorMode.Auto;
+    public CursorMode curMode = CursorMode.Auto;
+
     void Start()
     {
         Cursor.SetCursor(defaultMouse, hotSpot, curMode);
     }
-    
+
+    private void OnMouseOver()
+    {
+        Cursor.SetCursor(mouseOverMouse, hotSpot, curMode);
+
+    }
 }
