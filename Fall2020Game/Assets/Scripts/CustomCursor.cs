c1c78bc9824e52fc0859a3cf8f40c621f8a0558d
matt poast <matt.poast@gmail.com>
2020-10-18 23:24:41 -0600

    script for for cursor to change with dif tags, created playerCam

--- Fall2020Game/Assets/Scripts/CustomCursor.cs
+++ Fall2020Game/Assets/Scripts/CustomCursor.cs
@@ -1,19 +1,12 @@
 ﻿using UnityEngine;
-public class CursorChanger : MonoBehaviour
+public class CustomCursor : MonoBehaviour
 {
-    public Texture2D mouseOverMouse;
     public Texture2D defaultMouse;
     public Vector2 hotSpot = Vector2.zero;
     public CursorMode curMode = CursorMode.Auto;
-
     void Start()
     {
         Cursor.SetCursor(defaultMouse, hotSpot, curMode);
     }
-
-    private void OnMouseOver()
-    {
-        Cursor.SetCursor(mouseOverMouse, hotSpot, curMode);
-
-    }
 }
+
---
+++ Fall2020Game/Assets/Scripts/ObjectMouse.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+public class ObjectMouse : MonoBehaviour
+{
+    public Texture2D objectMouse;
+     public Texture2D defaultMouse;
+     public Vector2 hotSpot = Vector2.zero;
+     public CursorMode curMode = CursorMode.Auto;
+     private void OnMouseEnter()
+      {
+       if (gameObject.tag == "objectCursor")
+       {
+        Cursor.SetCursor(objectMouse, hotSpot, curMode);
+       }
+      }
+      private void OnMouseExit()
+      {
+       Cursor.SetCursor(defaultMouse, hotSpot, curMode);
+      }
+     }
