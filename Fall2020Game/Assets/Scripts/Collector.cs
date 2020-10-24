f510d5147cd5ff7e9eab3ae8d5f74e7cbaf54fb5
matt poast <matt.poast@gmail.com>
2020-10-23 19:20:46 -0600

    collecting script

---
+++ Fall2020Game/Assets/Scripts/Collector.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+
+public class Collector : MonoBehaviour
+{
+    public GameObject brain;
+    public int counter;
+
+    private void OnTriggerEnter(Collider other)
+    {
+         if (CompareTag("collectable"))
+         {
+             counter += 1; 
+             print(counter);
+             brain.SetActive(false);
+         }
+
+         if (counter == 5)
+         {
+             Debug.Log("Winner");
+         }
+    }
+}
+
+
