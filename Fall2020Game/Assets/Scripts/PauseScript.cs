2151d7b13c9e3205a71f7707dc0e9324a36090ed
matt poast <matt.poast@gmail.com>
2020-10-26 23:54:17 -0600

    PauseScript

---
+++ Fall2020Game/Assets/Scripts/PauseScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+public class PauseScript : MonoBehaviour
+{
+   public GameObject pauseScreen;
+   public bool gameIsPaused;
+
+   public void Start()
+   {
+      pauseScreen.SetActive(false);
+   }
+
+   public void Update()
+   {
+      if (Input.GetKeyDown(KeyCode.Escape))
+      {
+         gameIsPaused = !gameIsPaused;
+         Pause();
+      }
+   }
+
+   private void Pause()
+
+   {
+      if (gameIsPaused)
+      {
+         Time.timeScale = 0;
+pauseScreen.SetActive(true);
+      }
+      else
+      {
+         Time.timeScale = 1;
+      }
+   }
+}
