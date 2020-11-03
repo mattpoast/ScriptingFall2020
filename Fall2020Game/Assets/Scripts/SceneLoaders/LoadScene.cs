8383e32f83a8a2072b68db053f39cc2bf92b3534
matt poast <matt.poast@gmail.com>
2020-11-02 22:32:59 -0700

    Build Scene

---
+++ Fall2020Game/Assets/Scripts/SceneLoaders/LoadScene.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+   private void OnMouseDown()
+   {
+      SceneManager.LoadScene(1);
+      print ("working");
+   }
+}
