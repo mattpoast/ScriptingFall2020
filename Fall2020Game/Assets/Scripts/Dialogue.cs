78adb41591d61b7eff473f2e75f545c4d6d83a27
matt poast <matt.poast@gmail.com>
2020-11-03 22:15:05 -0700

    build new scene, dialogue script

---
+++ Fall2020Game/Assets/Scripts/Dialogue.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Net.Mime;
+using UnityEngine;
+
+public class Dialogue : MonoBehaviour
+{
+ public string lineOne = "what's that?", lineTwo = "idk";
+
+ private void OnMouseDown()
+ {
+  print(lineOne);
+  print(lineTwo);
+  
+  
+ }
+}
---
+++ Fall2020Game/Assets/Scripts/SceneLoaders/LoadSceneZero.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneZero : MonoBehaviour
+{
+    private void OnMouseDown()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
