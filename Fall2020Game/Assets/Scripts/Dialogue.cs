f93dc548fe9cffe6cdf9923062e698d61b2402ef
matt poast <matt.poast@gmail.com>
2020-11-04 16:36:22 -0700

    Working on scrip to randomly pick if bool is true or not

--- Fall2020Game/Assets/Scripts/Dialogue.cs
+++ Fall2020Game/Assets/Scripts/Dialogue.cs
@@ -1,16 +1,27 @@
 ﻿using System;
 using System.Net.Mime;
 using UnityEngine;
+using Random = System.Random;
 
 public class Dialogue : MonoBehaviour
 {
- public string lineOne = "what's that?", lineTwo = "idk";
-
- private void OnMouseDown()
+ public string lineOne = "what's that?", lineTwo = "I wonder what that is?";
+ public bool ageBeforeBeauty;
+ 
+ public void OnMouseDown()
  {
-  print(lineOne);
-  print(lineTwo);
-  
-  
+  RandomText();
+ }
+ private void RandomText()
+ {
+  if (ageBeforeBeauty)
+  {
+   print(lineOne);
+  }
+  else
+  {
+   print(lineTwo);
+  }
  }
 }
+
