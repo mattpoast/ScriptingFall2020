0747cc0e71dbf3a1cc56d7bdbf3092a0451b14ae
matt poast <matt.poast@gmail.com>
2020-11-08 23:55:45 -0700

    script to play video clip animation with alpha

---
+++ Fall2020Game/Assets/Scripts/PlayAnimation.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.Video;
+
+public class PlayAnimation : MonoBehaviour
+{
+  public VideoPlayer coffinDrop;
+
+  public void OnMouseDown()
+  {
+    coffinDrop.targetCameraAlpha = 0.5f;
+  }
+}
