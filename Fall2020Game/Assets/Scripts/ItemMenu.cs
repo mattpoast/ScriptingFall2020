93b114660faf18a27f8f90091b5de08f8cd85fa4
matt poast <matt.poast@gmail.com>
2020-11-09 23:34:40 -0700

    script for an item menu

---
+++ Fall2020Game/Assets/Scripts/ItemMenu.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+public class ItemMenu : MonoBehaviour
+{
+  public GameObject itemMenu;
+  public Vector3 up;
+  public Vector3 down;
+  public float speed = 5f;
+  private void Start()
+  {
+    itemMenu.SetActive(true);
+  }
+  private void OnMouseEnter()
+  { 
+    itemMenu.SetActive(true);
+    transform.Translate(Time.deltaTime * speed * up);
+    itemMenu.transform.Translate(Time.deltaTime * speed * up);
+  }
+  private void OnMouseExit()
+  {
+    transform.Translate(Time.deltaTime * speed * down);
+    itemMenu.transform.Translate(Time.deltaTime * speed * down);
+  }
+}
