f843cde76f965f16b84757d3401cfaef456196cd
matt poast <matt.poast@gmail.com>
2020-11-20 23:42:27 -0700

    nav agent and nav mesh script

--- Fall2020Game/Assets/Scripts/AiScript.cs
+++ Fall2020Game/Assets/Scripts/AiScript.cs
@@ -1,9 +1,19 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.AI;
 
 public class AiScript : MonoBehaviour
 {
-    public NavMeshAgent ai;
-    
-    
+    public NavMeshAgent agent;
+    public Transform player;
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        agent.destination = player.position;
+    }
 }
