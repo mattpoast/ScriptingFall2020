cb78735750bbae06d3fbd3b5f4fdbac6c59fbfa7
matt poast <matt.poast@gmail.com>
2020-12-04 22:33:16 -0700

    "endlessRunner" Script

---
+++ Fall2020Game/Assets/Scripts/SledGame/HillINstanciate.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class HillINstanciate : MonoBehaviour
+{
+    public Transform hillObj;
+    private Vector3 nextHillSpawn;
+    void Start()
+    {
+        nextHillSpawn.z = 9;
+        StartCoroutine(spawnHill());
+    }
+
+    IEnumerator spawnHill()
+    {
+        yield return new WaitForSeconds(1);
+        Instantiate(hillObj, nextHillSpawn, hillObj.rotation);
+        nextHillSpawn.z += 21;
+        nextHillSpawn.x += -21;
+        StartCoroutine(spawnHill());
+    }
+}
