619830ab563cc0476c610f16892130e04f064730
matt poast <matt.poast@gmail.com>
2020-12-06 23:57:49 -0700

    Script to spawn obstacles with delay

---
+++ Fall2020Game/Assets/Scripts/SledGame/TreeINstanciate.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class TreeINstanciate : MonoBehaviour
+{
+    public Transform tree;
+    public Vector3 treePlacement;
+
+    IEnumerator spawnTree()
+    {
+        yield return new WaitForSeconds(3);
+        Instantiate(tree, treePlacement, tree.rotation);
+    }
+}
