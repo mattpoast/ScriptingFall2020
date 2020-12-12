d87ba0e9ffa07051c111157e000bb54cc53bf554
matt poast <matt.poast@gmail.com>
2020-12-11 23:58:27 -0700

    new movement script for sled

--- Fall2020Game/Assets/Scripts/SledGame/ChairMover.cs
+++ Fall2020Game/Assets/Scripts/SledGame/ChairMover.cs
@@ -4,9 +4,20 @@ using UnityEngine;
 public class ChairMover : MonoBehaviour
 {
     public Rigidbody rb;
-
+    public float chairGas;
+    private int speed = 10;
+    private Vector3 movment;
+    public GameObject player;
     private void Start()
     {
-        rb.velocity = new Vector3(0,-3,3);
+        rb.velocity = new Vector3(0, -9, 9);
     }
+    public void Move()
+        {
+        
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"),0,0);
+        
+        player.transform.position += movement * speed * Time.deltaTime;
+        
+        }
 }
