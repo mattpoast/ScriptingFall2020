10804b15f22243eb498a53009e055f3d63586924
matt poast <matt.poast@gmail.com>
2020-12-14 23:53:36 -0700

    script to end the game when you die

--- Fall2020Game/Assets/Scripts/SledGame/Obsticles.cs
+++ Fall2020Game/Assets/Scripts/SledGame/Obsticles.cs
@@ -1,17 +1,10 @@
-﻿using System.Collections;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Obsticles : MonoBehaviour
 {
-    public GameObject tree;
-    public GameObject skis;
-    public float delay = 5f;
-    IEnumerator Start()
+    private void OnTriggerEnter(Collider other)
     {
-        while (true)
-        {
-            Instantiate(tree);
-            yield return new WaitForSeconds(delay);
-        }
+        Time.timeScale = 0;
+        print("Game Over");
     }
 }
