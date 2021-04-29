c833f6328f5e5e9022ef8423d22838e5a2a42412
matt poast <matt.poast@gmail.com>
2021-04-28 22:57:00 -0600

    PauseScript  for sled game, added pause song

---
+++ Sled/Assets/Scripts/pauseScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class pauseScript : MonoBehaviour
+{
+   public GameObject pauseScreen;
+      public bool gameIsPaused;
+      public AudioSource pauseSong;
+      public void Start()
+      {
+         pauseScreen.SetActive(false);
+      }
+      public void Update()
+      {
+         if (Input.GetKeyDown(KeyCode.Escape))
+         {
+            gameIsPaused = !gameIsPaused;
+            Pause();
+         }
+      }
+      private void Pause()
+      {
+         if (gameIsPaused)
+         {
+            Time.timeScale = 0;
+   pauseScreen.SetActive(true);
+   pauseSong.Play(1);
+         }
+         else
+         {
+            Time.timeScale = 1;
+            pauseScreen.SetActive(false);
+            pauseSong.Stop();
+         }
+      }
+}
