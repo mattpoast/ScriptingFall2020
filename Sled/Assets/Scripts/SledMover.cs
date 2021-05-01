11a692ff7a4725f985d24bc7a115f9bf37c71ce4
matt poast <matt.poast@gmail.com>
2021-05-01 10:44:52 -0600

    gravity to controller script

--- Sled/Assets/Scripts/SledMover.cs
+++ Sled/Assets/Scripts/SledMover.cs
@@ -5,17 +5,27 @@ public class SledMover : MonoBehaviour
         private Vector3 moveDirection;
         public CharacterController controller;
         public Vector3 sledPower;
+        public float g = 9.81f;
+        private bool isGrounded;
+        public float jumpForce = 100f;
         private void Start()
         {
             controller.velocity.Equals(sledPower);
         }
         public void Update()
         {
+            
             var moveSpeedInput = moveSpeed * Input.GetAxis("Horizontal");
     
-            moveDirection.Set(moveSpeedInput, 0, 0);
+            moveDirection.Set(moveSpeedInput, -g, 0);
              controller.Move(moveDirection * Time.deltaTime);
              controller.Move(sledPower * Time.deltaTime);
+
+             if (Input.GetKeyDown(KeyCode.Space))
+             {
+                 moveDirection.Set(moveSpeedInput, jumpForce, 0);
+             }
+             
         }
 
         
