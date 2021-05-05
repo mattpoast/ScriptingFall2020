fb764235e26e48e162d660a6f765e4f6eee501aa
matt poast <matt.poast@gmail.com>
2021-05-05 11:21:34 -0600

    Invincibility PowerUp Script

---
+++ Sled/Assets/Scripts/InvinciblePowerUp.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+public class InvinciblePowerUp : MonoBehaviour
+{
+    public GameObject star;
+    public bool starTrue;
+
+    public void Start()
+    {
+        starTrue = false;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        star.SetActive(false);
+        print("StarActive");
+        starTrue = true;
+        if (other.gameObject.CompareTag("Obstacle") && starTrue)
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
