3a9ff04a76394a7d08ed6482a43ca9cd327b7061
matt poast <matt.poast@gmail.com>
2021-05-12 13:23:17 -0600

    FreezeTag game, character controller script

---
+++ Freez Tag/Assets/Scripts/ThirdPerson.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+public class ThirdPerson : MonoBehaviour
+{
+    public float moveSpeed = 10f;
+    public float lookSpeed = 5f;
+    public Vector3 moveDirection;
+    public CharacterController controller;
+    public float gravity = -9.81f;
+    private float yDirection;
+    public float jumpPower = 15f;
+    void Update()
+    {
+        var moveSpeedInput = moveSpeed * Input.GetAxis("Horizontal");
+        var moveSpeedInputZ = moveSpeed * Input.GetAxis("Vertical");
+        moveDirection.Set(moveSpeedInput, yDirection,moveSpeedInputZ);
+        yDirection += gravity * Time.deltaTime;
+        controller.Move(moveDirection * Time.deltaTime);
+
+        if (controller.isGrounded && moveDirection.y <0)
+        {
+            yDirection = -1f;
+        }
+        if (Input.GetButtonDown("Jump"))
+        {
+            yDirection = jumpPower;
+        }
+    }
+}
