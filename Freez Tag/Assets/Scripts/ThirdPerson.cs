891bcd235b7a81551eae4417056ff81fb012793e
matt poast <matt.poast@gmail.com>
2021-05-17 00:09:10 -0600

    fixed bug where player would move in direction of camera without input

--- Freez Tag/Assets/Scripts/ThirdPerson.cs
+++ Freez Tag/Assets/Scripts/ThirdPerson.cs
@@ -2,19 +2,18 @@
 public class ThirdPerson : MonoBehaviour
 {
     public float moveSpeed = 10f;
-    public Vector3 moveDirection;
     public CharacterController controller;
     public float gravity = -9.81f;
     private float yDirection;
     public float jumpPower = 15f;
-    public float turnSmoothVelocity;
+    private float turnSmoothVelocity;
     public float turnSmoothTime = 0.1f;
     public Transform cam;
     void Update()
     {
         var moveSpeedInput = Input.GetAxisRaw("Horizontal");
          var moveSpeedInputZ = Input.GetAxisRaw("Vertical");
-         Vector3 direction = new Vector3(moveSpeedInput,yDirection, moveSpeedInputZ).normalized;
+         Vector3 direction = new Vector3(moveSpeedInput,0f, moveSpeedInputZ).normalized;
         
          yDirection += gravity * Time.deltaTime;
          
@@ -25,8 +24,8 @@ public class ThirdPerson : MonoBehaviour
                 turnSmoothTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
-            Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            controller.Move(moveDirection.normalized * moveSpeed * Time.deltaTime);
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDir.normalized * moveSpeed * Time.deltaTime);
         }
         
         if (controller.isGrounded && direction.y <0)
